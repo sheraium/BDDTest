96f32211ae67e42659750329422083de6719d39c
YongXiu <sheraium@gmail.com>
2018-12-16 22:20:34 +0800

    add feature

---
+++ UserApi.Tests/GetUserFeature.feature
@@ -0,0 +1,17 @@
+﻿Feature: GetUserFeature
+	In order to get user information from the API
+	As a API consumer
+	I want to be get users by ID
+
+@mytag
+Scenario: Get User by Id
+	Given that a user exists in the system
+	When I request to get the user by Id
+	Then the user should be returned in the response
+	And the response status code is "200 OK"
+
+Scenario: Get non-existing user by Id
+	Given that a user does not exist in the system
+	When I request to get the user by Id
+	Then no user should be returned in the response
+	And the response status code is "404 Not Found"
---
+++ UserApi.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UserApi.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UserApi.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad4f83b6-c379-435f-a2dc-1947689bc7fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

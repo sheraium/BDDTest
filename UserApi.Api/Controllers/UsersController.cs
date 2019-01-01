30030b994b5cfa4fab17366333d05060969b7df6
YongXiu <sheraium@gmail.com>
2019-01-01 15:16:26 +0800

    pass GetUserFeatureSteps

--- UserApi.Api/Controllers/UsersController.cs
+++ UserApi.Api/Controllers/UsersController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
 using UserApi.Data.Interfaces;
 
 namespace UserApi.Api.Controllers
@@ -14,7 +9,20 @@ namespace UserApi.Api.Controllers
 
         public UsersController(IUsersRepository usersRepository)
         {
-            _usersRepository = usersRepository; 
+            _usersRepository = usersRepository;
+        }
+
+        public IHttpActionResult GetUser(int id)
+        {
+            var user = _usersRepository.GetById(id);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
---
+++ UserApi.Tests/GetUserFeatureSteps.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Http;
+using System.Web.Http.Results;
+using TechTalk.SpecFlow;
+using UserApi.Api.Controllers;
+using UserApi.Data.Repositories;
+using UserApi.Entities;
+
+namespace UserApi.Tests
+{
+    [Binding]
+    public class GetUserFeatureSteps
+    {
+        private UsersController usersController;
+        private IHttpActionResult response;
+
+        [Given(@"that a user exists in the system")]
+        public void GivenThatAUserExistsInTheSystem()
+        {
+            InMemoryUsersRepository repository = new InMemoryUsersRepository();
+            Entities.User user = new User() { Id = 1, Email = "test@test.com", Name = "TestName", Surname = "TestSurnam" };
+            repository.Add(user);
+            usersController = new UsersController(repository);
+        }
+
+        [Given(@"that a user does not exist in the system")]
+        public void GivenThatAUserDoesNotExistInTheSystem()
+        {
+            InMemoryUsersRepository repository = new InMemoryUsersRepository();
+            usersController = new UsersController(repository);
+        }
+
+        [When(@"I request to get the user by Id")]
+        public void WhenIRequestToGetTheUserById()
+        {
+            response = usersController.GetUser(1);
+        }
+
+        [Then(@"the user should be returned in the response")]
+        public void ThenTheUserShouldBeReturnedInTheResponse()
+        {
+            var user = response as OkNegotiatedContentResult<User>;
+            Assert.AreEqual(user.Content.Id, 1);
+        }
+
+        [Then(@"the response status code is ""(.*)""")]
+        public void ThenTheResponseStatusCodeIs(string p0)
+        {
+            if (p0 == "200 OK")
+            {
+                var resp = response as OkNegotiatedContentResult<User>;
+                Assert.IsNotNull(resp);
+            }
+            else if (p0 == "404 Not Found")
+            {
+                var resp = response as NotFoundResult;
+                Assert.IsNotNull(resp);
+            }
+        }
+
+        [Then(@"no user should be returned in the response")]
+        public void ThenNoUserShouldBeReturnedInTheResponse()
+        {
+            var resp = response as NotFoundResult;
+            Assert.IsNotNull(resp);
+        }
+    }
+}

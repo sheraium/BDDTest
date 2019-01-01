459a73e68384b129cbc9348c82f669778c66477b
YongXiu <sheraium@gmail.com>
2019-01-01 14:46:05 +0800

    add UserApi.Api-skeleton

---
+++ UserApi.Api/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using UserApi.Data.Interfaces;
+
+namespace UserApi.Api.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private IUsersRepository _usersRepository = null;
+
+        public UsersController(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository; 
+        }
+    }
+}
